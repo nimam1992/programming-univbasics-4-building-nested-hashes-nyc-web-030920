0099a8bd8a1892af0550a43c62097ab1153a8c10
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-15 02:35:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # complexity.
 	monopoly = {:railroads=>{}}
 	monopoly[:railroads] = {:pieces=>{}}
-	monopoly[:railroads][:pieces]='4'
+	monopoly[:railroads][:pieces]=4
 	monopoly
 end
 
